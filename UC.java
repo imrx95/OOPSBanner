c7decd8c78e0808881855918350ae44c07f32c20
imrx95 <imrx95@gmail.com>
2026-03-11 10:05:18 +0530

    uc-3 added

--- UC.java
+++ UC.java
@@ -1,15 +1,17 @@
-public class UC {
-    public static void main(String[] args) {
-		System.out.println("OOPS BANNER APP :\n");
-        System.out.println("   ***    " + "   ***    " + "******    " + "   ***** ");
-		System.out.println(" **   **  " + " **   **  " + "**    **  " + " **      ");
-		System.out.println("**     ** " + "**     ** " + "**     ** " + "**       ");
-		System.out.println("**     ** " + "**     ** " + "**    **  " + " **      ");
-		System.out.println("**     ** " + "**     ** " + "******    " + "   ***   ");
-		System.out.println("**     ** " + "**     ** " + "**        " + "      ** ");
-		System.out.println("**     ** " + "**     ** " + "**        " + "       **");
-		System.out.println(" **   **  " + " **   **  " + "**        " + "      **");
-		System.out.println("   ***    " + "   ***    " + "**        " + " *****");
+/**
+ * Printing OOPS
+ * @author MOHAMED IMRAN S
 
+ */
+
+public class UC{
+    public static void main(String[] args) {
+        System.out.println(String.join(" ", "*****", " ","*****", " ", "*****", " ", "*****"));
+        System.out.println(String.join(" ", "*   *", " ", "*   *", " ", "*   *", " ", "*    "));
+        System.out.println(String.join(" ", "*   *", " ", "*   *", " ", "*   *", " ", "*    "));
+        System.out.println(String.join(" ", "*   *", " ", "*   *", " ", "*****", " ", "*****"));
+        System.out.println(String.join(" ", "*   *", " ", "*   *", " ", "*    ", " ", "    *"));
+        System.out.println(String.join(" ", "*   *", " ", "*   *", " ", "*    ", " ", "    *"));
+        System.out.println(String.join(" ", "*****", " ", "*****", " ", "*    ", " ", "*****"));                
     }
 }
